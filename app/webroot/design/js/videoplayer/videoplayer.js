b6ff952d7431995f6b510898e8f662d6eaac7714
Scoté <arnaud.scote@gmail>
2014-10-14 18:00:05 +0200

    correction player

--- app/webroot/design/js/videoplayer/videoplayer.js
+++ app/webroot/design/js/videoplayer/videoplayer.js
@@ -1,11 +1,8 @@
 $('document').ready(function(){
-    var player = $('#live_embed_player_flash')[0];
     $('#closePlayer').click(function(){
         $('#player').slideUp(1000,function(){
             $('#closePlayer').hide();
             $('#openPlayer').show();
-            var player = $('#live_embed_player_flash')[0];
-            player.pauseVideo();
         });
     });
 
@@ -13,10 +10,6 @@ $('document').ready(function(){
         $('#player').slideDown(1000,function(){
             $('#openPlayer').hide();
             $('#closePlayer').show();
-            player.playVideo();
         });
     });
-
-
-
 });
