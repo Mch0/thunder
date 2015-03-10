37062d246b622176c9ec8b89aa53456bd9c39205
Arnaud SCOTE <arnaud.scote@netapsys.fr>
2015-03-10 13:46:17 +0100

    videoplayer js remove play stop add mute at start

--- app/webroot/design/js/videoplayer/videoplayer.js
+++ app/webroot/design/js/videoplayer/videoplayer.js
@@ -1,25 +1,43 @@
 BCL = {};
-//handler (gestionnaire) d'événement template loaded
+
+
+BCL.onTemplateLoaded = function (expID) {
+    var player = brightcove.getPlayer(expID);
+    var video = player.getModule(APIModules.VIDEO_PLAYER);
+    var exp = player.getModule(APIModules.EXPERIENCE);
+    exp.addEventListener(BCExperienceEvent.TEMPLATE_READY,
+        function () {
+            // sets the volume to 50%
+            console.log("otot");
+            video.mute(true);
+        });
+};
+
+/*//handler (gestionnaire) d'événement template loaded
 BCL.onTemplateLoad = function (experienceID) {
 
-  // permet d'obtenir des références au lecteur et aux événements et modules de l'API.
+    // permet d'obtenir des références au lecteur et aux événements et modules de l'API.
     BCL.player = brightcove.api.getExperience(experienceID);
-      BCL.APIModules = brightcove.api.modules.APIModules;
-        BCL.adEvent = brightcove.api.events.AdEvent;
-          BCL.captionsEvent = brightcove.api.events.CaptionsEvent;
-            BCL.contentEvent = brightcove.api.events.ContentEvent;
-              BCL.cuePointEvent = brightcove.api.events.CuePointEvent;
-              BCL.mediaEvent = brightcove.api.events.MediaEvent;
-              console.log(BCL);
-              };
-              BCL.onTemplateReady = function (evt) {
-                BCL.videoPlayer = BCL.player.getModule(BCL.APIModules.VIDEO_PLAYER);
-                var test =  BCL.player.getModule(BCL.APIModules.VIDEO_PLAYER);
-                };
+    BCL.APIModules = brightcove.api.modules.APIModules;
+    BCL.adEvent = brightcove.api.events.AdEvent;
+    BCL.captionsEvent = brightcove.api.events.CaptionsEvent;
+    BCL.contentEvent = brightcove.api.events.ContentEvent;
+    BCL.cuePointEvent = brightcove.api.events.CuePointEvent;
+    BCL.mediaEvent = brightcove.api.events.MediaEvent;
+};
 
-$('document').ready(function(){
-    $('#closePlayer').click(function(){
-        $('#player').slideUp(1000,function(){
+BCL.onTemplateReady = function (evt) {
+    BCL.videoPlayer = BCL.player.getModule(BCL.APIModules.VIDEO_PLAYER);
+    var monPLayer = BCL.videoPlayer;
+    var test = BCL.player.getModule(BCL.APIModules.VIDEO_PLAYER);
+
+};*/
+
+$('document').ready(function () {
+
+    console.log(BCL);
+    $('#closePlayer').click(function () {
+        $('#player').slideUp(1000, function () {
             $('#closePlayer').hide();
             $('#openPlayer').show();
             //player.stopVideo();
@@ -27,8 +45,8 @@ $('document').ready(function(){
         });
     });
 
-    $('#openPlayer').click(function(){
-        $('#player').slideDown(1000,function(){
+    $('#openPlayer').click(function () {
+        $('#player').slideDown(1000, function () {
             $('#openPlayer').hide();
             $('#closePlayer').show();
             //player.playVideo();
